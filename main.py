71cfe2ef03a5738909d84cfe546d598b5d7dec1b
hex-plex <sendhilsom7@gmail.com>
2022-12-30 22:20:42 +0530

    added files

---
+++ main.py
@@ -0,0 +1,51 @@
+from localization import fetch_coordinates, imageServer
+from control import altitude_cnt, cartesian_cnt
+from comm import Drone
+from utils import sleepTimer
+from trajectory import BasicTrajectoryServer as trajectoryServer # Add task specific trajectory servers
+
+def main():
+	drone = Drone(
+			# ip address,
+			# port number,
+			# timeout
+			)
+	drone.prepare()
+	drone.armTakeOff()
+
+	trajectory_server = trajectoryServer(r, n) ## Define shape using polygon
+
+	img_server = imageServer()
+	img_server.start()
+	coordinates = fetch_coordinates(img_server)
+
+	drone.getControl().start()
+	sleep_timer = sleepTimer(50) # rate
+	controllers = [
+			altitude_cnt(),
+			cartesian_cnt()
+			]
+
+	while drone.ok():
+		coordinates.fetch()
+		drone_info = drone.getState()
+		desired_states = trajectory_server.fetch_next_goal(coordinates, drone_info)
+		command = []
+		for controller in controllers:
+			command += controller.update(coordinates, drone_info, desired_states)
+		command = drone.command_preprocess(command)
+		drone.sendCommand(command)
+		sleep_timer.sleep()
+	finally:
+		final_command = []
+		for cont in controllers:
+			final_command += cont.kill()
+		final_command = drone.command_preprocess(final_command)
+		drone.sendCommand(command)
+		sleep_timer.sleep()
+		drone.land()
+		sleep_timer.sleep()
+	del drone
+	del trajectory_server
+	del controllers
+	print("completed run")
