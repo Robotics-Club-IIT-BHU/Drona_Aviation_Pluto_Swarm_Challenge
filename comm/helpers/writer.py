207fb389ffc6bad0d6e2ed7a5f591176156d8ac1
EPSA <120899038+ePSA-stACKoverFLoW@users.noreply.github.com>
2023-01-17 17:42:23 +0530

    Update writer.py

--- comm/helpers/writer.py
+++ comm/helpers/writer.py
@@ -4,10 +4,12 @@ class Writer:
     def __init__(self,socket):
         self.socket=socket
         self.MSP_HEADER="$M<"
+        #default value of MSP packets
         self.MSP_SET_COMMAND = 217
         self.MSP_SET_RAW_RC=200
         self.MSP_ACC_TRIM=240
-   
+    
+    #function for package creation for a given payload
     def createPacketMSP(self,msp,payload):
         bf=[]
         for k in self.MSP_HEADER:
@@ -16,7 +18,7 @@ class Writer:
         if len(payload)==0:
             pl_size=0& 0xFF
         else:
-            pl_size=len(payload) & 0xFF                         #package creation
+            pl_size=len(payload) & 0xFF                         
         bf.append(pl_size)
         check_sum^=(pl_size & 0xFF)
 
@@ -30,16 +32,18 @@ class Writer:
         bf.append(check_sum)
         return(bf)
 
-    def sendRequestMSP_SET_COMMAND(self,commandType):                                                    #set command
+    #sending request for packet creation
+    def sendRequestMSP_SET_COMMAND(self,commandType):                                                  
         payload=[commandType&0xff,0,0,0,0,0,0,0]
         self.sendRequestMSP(self.createPacketMSP(self.MSP_SET_COMMAND,payload))
 
+    #sending request for packet creation
     def sendRequestMSP_SET_RAW_RC(self,channels):
         index=0
         rc_signals=[i for i in range(16)]
         for i in range(0,8,1):
             rc_signals[index]=channels[i]&0xff
-            index+=1                                                        #packet ki values 
+            index+=1                                                        
             rc_signals[index]=(channels[i]>>8)& 0xff
             index+=1
         self.sendRequestMSP(self.createPacketMSP(self.MSP_SET_RAW_RC,rc_signals))
@@ -53,23 +57,10 @@ class Writer:
                 arr.append(np.uint8(d))
         self.socket.send(arr)
 
+    #sending request for packet creation
     def sendRequestMSP_ACC_TRIM(self):
         self.sendRequestMSP(self.createPacketMSP(self.MSP_ACC_TRIM, []))
 
     def sendRequestMSP_GET_DEBUG(self,requests):
         for k in requests:
             self.sendRequestMSP(self.createPacketMSP(k, []))
-
-
-
-
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     while 1:
-#         my_server()
