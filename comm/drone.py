8b725f447e526f6669826977eda975bb503ca194
Ankur Agrawal <ankuragrawal9455@gmail.com>
2023-01-28 02:47:30 +0530

    Formatting Document

--- comm/drone.py
+++ comm/drone.py
@@ -14,25 +14,26 @@ MSP_ANALOG = 110
 
 
 class Drone:
-    #setting up the initial condition of the drone i.e. the initial condition of roll pitch yaw and initial socket parameters i.e. the host and port and other parameters
-    def __init__(self,host,port,timeout):
-        self.HOST=host
-        self.PORT=port
-        self.timeout=timeout
-        self.runThreads=True
-        self.socketStable=True
-        self.SOCKET=None
-        while self.SOCKET is None: self.start_server()
-        self.Reader=Reader(socket=self.SOCKET)
-        self.Writer=Writer(socket=self.SOCKET)
-        self.userRC=[1500,1500,1500,1500,1000,1000,1000,1000]
-        self.userRCAP=[1500,1500,1500,1500]
-        self.isAutoPilotOn=False
-        self.commandType=0
+    # setting up the initial condition of the drone i.e. the initial condition of roll pitch yaw and initial socket parameters i.e. the host and port and other parameters
+    def __init__(self, host, port, timeout):
+        self.HOST = host
+        self.PORT = port
+        self.timeout = timeout
+        self.runThreads = True
+        self.socketStable = True
+        self.SOCKET = None
+        while self.SOCKET is None:
+            self.start_server()
+        self.Reader = Reader(socket=self.SOCKET)
+        self.Writer = Writer(socket=self.SOCKET)
+        self.userRC = [1500, 1500, 1500, 1500, 1000, 1000, 1000, 1000]
+        self.userRCAP = [1500, 1500, 1500, 1500]
+        self.isAutoPilotOn = False
+        self.commandType = 0
         self.start_threads()
-        self.cmd=Data()
-    
-    #starting the server for communication through socket
+        self.cmd = Data()
+
+    # starting the server for communication through socket
     def start_server(self):
         count = 3
         while count:
@@ -58,12 +59,12 @@ class Drone:
         if not count:
             exit(0)
 
-    #reader thread function
+    # reader thread function
     def reader(self):
         while self.runThreads:
             self.Reader.read_frame()
 
-    #writer thread function
+    # writer thread function
     def writer(self):
         print("Writer thread started...")
         request = [MSP_RC, MSP_ATTITUDE, MSP_RAW_IMU, MSP_ALTITUDE, MSP_ANALOG]
@@ -74,14 +75,14 @@ class Drone:
                 if acquired:
                     droneRC = self.userRC
 
-                    #autopilot condition
-                    if self.isAutoPilotOn and droneRC[7]==1500:
+                    # autopilot condition
+                    if self.isAutoPilotOn and droneRC[7] == 1500:
                         droneRC[0] += self.userRCAP[0] - 1500
                         droneRC[1] += self.userRCAP[1] - 1500
                         droneRC[2] += self.userRCAP[2] - 1500
                         droneRC[3] += self.userRCAP[3] - 1500
-                                         
-                    #calling the sendRequestMSP_SET_RAW_RC function in the helpers folder
+
+                    # calling the sendRequestMSP_SET_RAW_RC function in the helpers folder
                     self.Writer.sendRequestMSP_SET_RAW_RC(droneRC)
                     self.Writer.sendRequestMSP_GET_DEBUG(request)
 
@@ -97,15 +98,14 @@ class Drone:
                         self.commandType = 0
 
                     self.lock.release()
-            #error in socket maybe disconnected or unstable
+            # error in socket maybe disconnected or unstable
             except socket.error as e:
                 self.socketStable = False
                 print("Cannot write to thread: %s" % (e))
             time.sleep(0.022)
-        
-    
-    #sending the command
-    def sendCommand(self,data):
+
+    # sending the command
+    def sendCommand(self, data):
         self.userRC[0] = data.rcRoll
         self.userRC[1] = data.rcPitch
         self.userRC[2] = data.rcThrottle
@@ -116,24 +116,24 @@ class Drone:
         self.userRC[7] = data.rcAUX4
         self.isAutoPilotOn = data.isAutoPilotOn
         self.commandType = data.commandType
-    
-    #publishing the roll pitch yaw values 
-    def command_preprocess(self,arr):
-        self.cmd.rcRoll=arr[0]
-        self.cmd.rcPitch=arr[1]
-        self.cmd.rcThrottle=arr[2]
-        self.cmd.rcYaw=arr[3]
+
+    # publishing the roll pitch yaw values
+    def command_preprocess(self, arr):
+        self.cmd.rcRoll = arr[0]
+        self.cmd.rcPitch = arr[1]
+        self.cmd.rcThrottle = arr[2]
+        self.cmd.rcYaw = arr[3]
         return self.cmd
-    
-    #starting the reader and writer threads
+
+    # starting the reader and writer threads
     def start_threads(self):
         self.lock = threading.Lock()
         self.t1 = threading.Thread(target=self.writer)
         self.t2 = threading.Thread(target=self.reader)
         self.t1.start()
         self.t2.start()
-    
-    #arming the drone
+
+    # arming the drone
     def arm(self):
         self.reset()
         self.cmd.rcRoll = 1500
@@ -145,7 +145,7 @@ class Drone:
         self.sendCommand(self.cmd)
         time.sleep(1)
 
-    #this function is called while taking off
+    # this function is called while taking off
     def box_arm(self):
         self.cmd.rcRoll = 1500
         self.cmd.rcYaw = 1500
@@ -163,15 +163,15 @@ class Drone:
         self.sendCommand(self.cmd)
         time.sleep(0.5)
 
-    #increasing the throttle to increase the height 
+    # increasing the throttle to increase the height
     def increase_height(self):
         self.cmd.rcThrottle = 2000
 
-    #decreasing the throttle to decrease the height
+    # decreasing the throttle to decrease the height
     def decrease_height(self):
         self.cmd.rcThrottle = 1300
 
-    #the take off function
+    # the take off function
     def take_off(self):
         self.reset()
         self.disarm()
@@ -182,31 +182,31 @@ class Drone:
         #     self.increase_height()
         # self.reset()
 
-    #landing the drone 
+    # landing the drone
     def land(self):
         self.decrease_height()
-    
-    #bringing the drone to its default state
+
+    # bringing the drone to its default state
     def reset(self):
         self.cmd.rcRoll = 1500
         self.cmd.rcThrottle = 1500
         self.cmd.rcPitch = 1500
         self.cmd.rcYaw = 1500
         self.cmd.commandType = 0
-    
-    #getting the values of the roll pitch and yaw i.e. the current status of the drone
+
+    # getting the values of the roll pitch and yaw i.e. the current status of the drone
     def getState(self):
         return [
             self.Reader.ATTITUDE["roll"],
             self.Reader.ATTITUDE["pitch"],
             self.Reader.ATTITUDE["yaw"],
         ]
-    
-    #checking if the threads and socket are alright
+
+    # checking if the threads and socket are alright
     def ok(self):
         return self.runThreads and self.socketStable
-    
-    #closing everything 
+
+    # closing everything
     def __del__(self):
         self.land()
         self.disarm()
--- comm/teleop/key_handling.py
+++ comm/teleop/key_handling.py
@@ -1,146 +1,158 @@
 import time
 
-class Data():
-	#defining the initial state of the drone with roll pitch and yaw value 
-	def __init__(self):
-		self.rcRoll =1500
-		self.rcPitch = 1500
-		self.rcYaw =1500
-		self.rcThrottle =1500
-		self.rcAUX1 =1500
-		self.rcAUX2 =1500
-		self.rcAUX3 =1500
-		self.rcAUX4 =1000
-		self.commandType = 0
-		self.trim_roll = 0
-		self.trim_pitch = 0
-		self.isAutoPilotOn = 0
 
-class send_data():
-	"""docstring for request_data"""
-	def __init__(self,drone):
-		self.key_value =0
-		self.cmd=Data()
-		self.drone=drone
+class Data:
+    # defining the initial state of the drone with roll pitch and yaw value
+    def __init__(self):
+        self.rcRoll = 1500
+        self.rcPitch = 1500
+        self.rcYaw = 1500
+        self.rcThrottle = 1500
+        self.rcAUX1 = 1500
+        self.rcAUX2 = 1500
+        self.rcAUX3 = 1500
+        self.rcAUX4 = 1000
+        self.commandType = 0
+        self.trim_roll = 0
+        self.trim_pitch = 0
+        self.isAutoPilotOn = 0
 
-	#arming the drone 
-	def arm(self):
-		self.cmd.rcRoll=1500
-		self.cmd.rcYaw=1500
-		self.cmd.rcPitch =1500
-		self.cmd.rcThrottle =1000
-		self.cmd.rcAUX4 =1500
-		self.cmd.isAutoPilotOn = 0
-		self.drone.sendCommand(self.cmd)
-		time.sleep(1)
 
-	#this is the function that is called while taking off
-	def box_arm(self):
-		self.cmd.rcRoll=1500
-		self.cmd.rcYaw=1500
-		self.cmd.rcPitch =1500
-		self.cmd.rcThrottle =1500
-		self.cmd.rcAUX4 =1500
-		self.cmd.isAutoPilotOn = 0
-		self.drone.sendCommand(self.cmd)
-		time.sleep(0.5)
+class send_data:
+    """docstring for request_data"""
 
-	#diarming the drone 
-	def disarm(self):
-		self.cmd.rcThrottle =1300
-		self.cmd.rcAUX4 = 1200
-		self.drone.sendCommand(self.cmd)
-		time.sleep(0.5)
+    def __init__(self, drone):
+        self.key_value = 0
+        self.cmd = Data()
+        self.drone = drone
 
-	#identify the key pressed and then processing the respective command by calling the proper function
-	def indentify_key(self, msg):
-		self.key_value = msg
+    # arming the drone
+    def arm(self):
+        self.cmd.rcRoll = 1500
+        self.cmd.rcYaw = 1500
+        self.cmd.rcPitch = 1500
+        self.cmd.rcThrottle = 1000
+        self.cmd.rcAUX4 = 1500
+        self.cmd.isAutoPilotOn = 0
+        self.drone.sendCommand(self.cmd)
+        time.sleep(1)
 
-		if self.key_value == 70:
-			if(self.cmd.rcAUX4 == 1500):
-				self.disarm()
-			else:
-				self.arm()
-		elif self.key_value == 10:
-			self.forward()
-		elif self.key_value == 30:
-			self.left()
-		elif self.key_value == 40:
-			self.right()
-		elif self.key_value == 80:
-			self.reset()
-		elif self.key_value == 90:
-			if(self.cmd.isAutoPilotOn == 1):
-				self.cmd.isAutoPilotOn = 0
-			else:
-				self.cmd.isAutoPilotOn = 1
-		elif self.key_value == 50:
-			self.increase_height()
-		elif self.key_value == 60:
-			self.decrease_height()
-		elif self.key_value == 110:
-			self.backward()
-		elif self.key_value == 130:
-			self.take_off()
-		elif self.key_value == 140:
-			self.land()
-		elif self.key_value == 150:
-			self.left_yaw()
-		elif self.key_value == 160:
-			self.right_yaw()
-		self.drone.sendCommand(self.cmd)
+    # this is the function that is called while taking off
+    def box_arm(self):
+        self.cmd.rcRoll = 1500
+        self.cmd.rcYaw = 1500
+        self.cmd.rcPitch = 1500
+        self.cmd.rcThrottle = 1500
+        self.cmd.rcAUX4 = 1500
+        self.cmd.isAutoPilotOn = 0
+        self.drone.sendCommand(self.cmd)
+        time.sleep(0.5)
 
-	#defining the forward function for drone 
-	def forward(self):
-		self.cmd.rcPitch =1600
-		self.drone.sendCommand(self.cmd)
-	#defining the backward function for drone 
-	def backward(self):
-		self.cmd.rcPitch =1400
-		self.drone.sendCommand(self.cmd)
-	#defining the left movement 
-	def left(self):
-		self.cmd.rcRoll =1400
-		self.drone.sendCommand(self.cmd)
-	#defining the right movement 
-	def right(self):
-		self.cmd.rcRoll =1600
-		self.drone.sendCommand(self.cmd)
-	#decreasing the yaw value of drone 
-	def left_yaw(self):
-		self.cmd.rcYaw = 1200
-		self.drone.sendCommand(self.cmd)
-	#increasing the yaw value of drone 
-	def right_yaw(self):
-		self.cmd.rcYaw = 1800
-		self.drone.sendCommand(self.cmd)
-	#resetting the drone to its default state 
-	def reset(self):
-		self.cmd.rcRoll =1500
-		self.cmd.rcThrottle =1600
-		self.cmd.rcPitch =1500
-		self.cmd.rcYaw = 1500
-		self.cmd.commandType = 0
-		self.drone.sendCommand(self.cmd)
-	
-	#increasing the height of the drone by decreasing the throttle
-	def increase_height(self):
-		self.cmd.rcThrottle = 2100
-		self.drone.sendCommand(self.cmd)
-	#decreasing the height of drone by decreasing the throttle
-	def decrease_height(self):
-		self.cmd.rcThrottle =1300
-		self.drone.sendCommand(self.cmd)
-	#taking off function
-	def take_off(self):
-		self.disarm()
-		self.box_arm()
-		# for i in range(6500000):
-		# 	self.increase_height()
-		# self.reset()
-		self.cmd.commandType = 1
-		self.drone.sendCommand(self.cmd)
-	#landing of drone 
-	def land(self):
-		self.cmd.commandType = 2
-		self.drone.sendCommand(self.cmd)
+    # diarming the drone
+    def disarm(self):
+        self.cmd.rcThrottle = 1300
+        self.cmd.rcAUX4 = 1200
+        self.drone.sendCommand(self.cmd)
+        time.sleep(0.5)
+
+    # identify the key pressed and then processing the respective command by calling the proper function
+    def indentify_key(self, msg):
+        self.key_value = msg
+
+        if self.key_value == 70:
+            if self.cmd.rcAUX4 == 1500:
+                self.disarm()
+            else:
+                self.arm()
+        elif self.key_value == 10:
+            self.forward()
+        elif self.key_value == 30:
+            self.left()
+        elif self.key_value == 40:
+            self.right()
+        elif self.key_value == 80:
+            self.reset()
+        elif self.key_value == 90:
+            if self.cmd.isAutoPilotOn == 1:
+                self.cmd.isAutoPilotOn = 0
+            else:
+                self.cmd.isAutoPilotOn = 1
+        elif self.key_value == 50:
+            self.increase_height()
+        elif self.key_value == 60:
+            self.decrease_height()
+        elif self.key_value == 110:
+            self.backward()
+        elif self.key_value == 130:
+            self.take_off()
+        elif self.key_value == 140:
+            self.land()
+        elif self.key_value == 150:
+            self.left_yaw()
+        elif self.key_value == 160:
+            self.right_yaw()
+        self.drone.sendCommand(self.cmd)
+
+    # defining the forward function for drone
+    def forward(self):
+        self.cmd.rcPitch = 1600
+        self.drone.sendCommand(self.cmd)
+
+    # defining the backward function for drone
+    def backward(self):
+        self.cmd.rcPitch = 1400
+        self.drone.sendCommand(self.cmd)
+
+    # defining the left movement
+    def left(self):
+        self.cmd.rcRoll = 1400
+        self.drone.sendCommand(self.cmd)
+
+    # defining the right movement
+    def right(self):
+        self.cmd.rcRoll = 1600
+        self.drone.sendCommand(self.cmd)
+
+    # decreasing the yaw value of drone
+    def left_yaw(self):
+        self.cmd.rcYaw = 1200
+        self.drone.sendCommand(self.cmd)
+
+    # increasing the yaw value of drone
+    def right_yaw(self):
+        self.cmd.rcYaw = 1800
+        self.drone.sendCommand(self.cmd)
+
+    # resetting the drone to its default state
+    def reset(self):
+        self.cmd.rcRoll = 1500
+        self.cmd.rcThrottle = 1600
+        self.cmd.rcPitch = 1500
+        self.cmd.rcYaw = 1500
+        self.cmd.commandType = 0
+        self.drone.sendCommand(self.cmd)
+
+    # increasing the height of the drone by decreasing the throttle
+    def increase_height(self):
+        self.cmd.rcThrottle = 2100
+        self.drone.sendCommand(self.cmd)
+
+    # decreasing the height of drone by decreasing the throttle
+    def decrease_height(self):
+        self.cmd.rcThrottle = 1300
+        self.drone.sendCommand(self.cmd)
+
+    # taking off function
+    def take_off(self):
+        self.disarm()
+        self.box_arm()
+        # for i in range(6500000):
+        # 	self.increase_height()
+        # self.reset()
+        self.cmd.commandType = 1
+        self.drone.sendCommand(self.cmd)
+
+    # landing of drone
+    def land(self):
+        self.cmd.commandType = 2
+        self.drone.sendCommand(self.cmd)
