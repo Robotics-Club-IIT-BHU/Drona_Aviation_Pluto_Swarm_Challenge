10a50f09f0223a2b7826993c242587603b03da04
DivyamTaneja <109144129+DivyamTaneja@users.noreply.github.com>
2023-01-29 16:30:44 +0530

    Add files via upload

---
+++ localization/transformation.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+class Transformation:
+    def inversePerspective(self, rvec, tvec):
+        R, _ = cv2.Rodrigues(rvec)
+        R = np.matrix(R).T
+        invTvec = np.dot(-R, np.matrix(tvec))
+        invRvec, _ = cv2.Rodrigues(R)
+        return invRvec, invTvec
+    def relativePosition(self, rvec1, tvec1, rvec2, tvec2):
+        rvec1, tvec1 = rvec1.reshape((3, 1)), tvec1.reshape((3, 1))
+        rvec2, tvec2 = rvec2.reshape((3, 1)), tvec2.reshape((3, 1))
+        invRvec, invTvec = self.inversePerspective(rvec2, tvec2)
+        info = cv2.composeRT(rvec1, tvec1, invRvec, invTvec)
+        composedRvec, composedTvec = info[0], info[1]
+        composedRvec = composedRvec.reshape((3, 1))
+        composedTvec = composedTvec.reshape((3, 1))
+        return composedRvec, composedTvec
