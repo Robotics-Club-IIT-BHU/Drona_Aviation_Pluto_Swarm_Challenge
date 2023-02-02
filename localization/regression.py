130d2546fdc1f10966c9052f3b42032c37761602
DivyamTaneja <109144129+DivyamTaneja@users.noreply.github.com>
2023-02-03 02:41:17 +0530

    Update regression.py

--- localization/regression.py
+++ localization/regression.py
@@ -1,8 +1,10 @@
+import os
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
+dir = os.getcwd()
 class Regression:
 	def __init__(self):
 		xy_lin1, xy_poly2, xy_lin2 = self.train_model(xy_X, xy_Y)
@@ -31,9 +33,9 @@ class Regression:
 		pos = np.concatenate((xy,z), axis=-1).flatten()
 		return pos
 
-xy_dataset = pd.read_csv('dataset/xy_dataset.csv')
+xy_dataset = pd.read_csv(dir+"/localization/dataset/xy_dataset.csv")
 xy_X = xy_dataset.iloc[:, :-2].values
 xy_Y = xy_dataset.iloc[:, -2:].values
-z_dataset = pd.read_csv('dataset/z_dataset.csv')
+z_dataset = pd.read_csv(dir+"/localization/dataset/z_dataset.csv")
 z_X = z_dataset.iloc[:, :-1].values
 z_Y = z_dataset.iloc[:, -1:].values
